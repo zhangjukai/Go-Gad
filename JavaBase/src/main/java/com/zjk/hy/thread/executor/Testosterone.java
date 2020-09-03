d10f001116349d43e886d9ee2c5359ff72b29c2d
zhangjukai <jk_zhangjukai@163.com>
2020-09-03 17:00:29 +0800

    补一下线程池的内容

---
+++ JavaBase/src/main/java/com/zjk/hy/thread/executor/Testosterone.java
@@ -0,0 +1,18 @@
+package com.zjk.hy.thread.executor;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Testosterone {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        ArrayList list = new ArrayList();
+        for (int i = 0; i < 100000; i++) {
+            executorService.submit(()->list.add(new Random().nextInt(100)));
+        }
+        System.out.println(list.size());
+        executorService.shutdown();
+    }
+}
