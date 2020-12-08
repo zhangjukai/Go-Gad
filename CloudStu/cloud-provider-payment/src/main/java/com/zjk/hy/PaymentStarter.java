4b887667667cca7f86e0cd0f87c08f7b4fd182c2
zhangjukai <jk_zhangjukai@163.com>
2020-12-08 20:38:12 +0800

    。。。。。

--- CloudStu/cloud-provider-payment/src/main/java/com/zjk/hy/PaymentStarter.java
+++ CloudStu/cloud-provider-payment/src/main/java/com/zjk/hy/PaymentStarter.java
@@ -3,13 +3,26 @@ package com.zjk.hy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @MapperScan(value = "com.zjk.hy.dao")
 public class PaymentStarter {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext applicationContext = SpringApplication.run(PaymentStarter.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
